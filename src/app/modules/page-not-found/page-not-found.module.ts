f0a75d62ffd2fdda97e0b158107dc6dcd890dac7
Felix J. Cruz Ortiz <fcruz@nd-soft.com>
2023-11-10 07:10:23 -0400

    Delete unnecesary routing module

--- src/app/modules/page-not-found/page-not-found.module.ts
+++ src/app/modules/page-not-found/page-not-found.module.ts
@@ -1,14 +1,12 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { PageNotFoundComponent } from "./page-not-found.component";
-import { PageNotFoundRoutingModule } from "./page-not-found.routing";
 import { RouterModule } from "@angular/router";
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
-    PageNotFoundRoutingModule
   ],
   declarations: [PageNotFoundComponent],
 })
